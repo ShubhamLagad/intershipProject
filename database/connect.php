c078dd48c8915960f954b4de896b7e8f9b8afbbe
ShubhamLagad <shubhamlagad2000@gmail.com>
2022-01-03 13:24:18 +0530

    c

--- database/connect.php
+++ database/connect.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "intership";
+    $host = " sql5.freesqldatabase.com";
+    $user = "sql5462924";
+    $password = "mHxfhngyJX";
+    $database = "sql5462924";
 
     $con = mysqli_connect($host,$user,$password,$database);
     // if($con)
